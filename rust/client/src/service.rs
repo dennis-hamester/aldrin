1da009507e8def268c12b12c7773dc020587acae
Dennis Hamester <dhamester@jusst.de>
2020-02-11 16:02:08 +0100

    client: Document service module

--- rust/client/src/service.rs
+++ rust/client/src/service.rs
@@ -7,6 +7,45 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 use uuid::Uuid;
 
+/// Owned service.
+///
+/// Services are always associated with an [`Object`](super::Object) and created by
+/// [`Object::create_service`](super::Object::create_service). Services are destroyed again by
+/// calling [`destroy`](Service::destroy), by dropping them, or implicitly when the
+/// [`Object`](super::Object) is destroyed.
+///
+/// To handle incoming function calls, iterate asynchronously over a service with the [`Stream`]
+/// trait, which then yields [`FunctionCall`s](FunctionCall).
+///
+/// Events can be emitted directly with [`Handle::emit_event`]. This is available on [`Handle`],
+/// because usually [`Service`] is borrowed mutably to wait for function calls. The [`ServiceId`]
+/// required for [`Handle::emit_event`] can be obtained with e.g. [`Service::id`].
+///
+/// # Examples
+///
+/// ```no_run
+/// // For StreamExt::next()
+/// use futures::stream::StreamExt;
+///
+/// // Create a new service with a random UUID.
+/// let mut service = object.create_service(ServiceUuid(Uuid::new_v4()), FIFO_SIZE).await?;
+///
+/// // Handle next function call.
+/// if let Some(call) = service.next().await {
+///     match call.id {
+///         1 => {
+///             // ...
+///             handle.emit_event(service.id(), 1, Value::None).await?;
+///             call.reply.ok(res).await?
+///         }
+///
+///         _ => call.reply.invalid_function().await?,
+///     }
+/// }
+///
+/// // Destroy the service again.
+/// service.destroy().await?;
+/// ```
 #[derive(Debug)]
 pub struct Service {
     id: ServiceId,
@@ -28,14 +67,21 @@ impl Service {
         }
     }
 
+    /// Returns the id of the service.
     pub fn id(&self) -> ServiceId {
         self.id
     }
 
+    /// Returns a handle to the client that was used to create the service.
+    ///
+    /// `None` is returned after the [`Service`] has been manually [destroyed](Service::destroy).
     pub fn handle(&self) -> Option<&Handle> {
         self.inner.as_ref().map(|i| &i.client)
     }
 
+    /// Destroys the service.
+    ///
+    /// If the service has already been destroyed, [`Error::InvalidService`] is returned.
     pub async fn destroy(&mut self) -> Result<(), Error> {
         let inner = self.inner.as_mut().ok_or(Error::InvalidService(self.id))?;
         let res = inner.client.destroy_service(self.id).await;
@@ -78,11 +124,20 @@ struct Inner {
     function_calls: Receiver<(u32, Value, u32)>,
 }
 
+/// Id of a service.
+///
+/// A [`ServiceId`] consists of an arbitrary UUID, and a cookie chosen by the server. The
+/// combination of both is unique at all times across the whole Aldrin bus.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[non_exhaustive]
 pub struct ServiceId {
+    /// Object id to which the service belongs.
     pub object_id: ObjectId,
+
+    /// UUID of the service.
     pub uuid: ServiceUuid,
+
+    /// Cookie of the service.
     pub cookie: ServiceCookie,
 }
 
@@ -96,6 +151,7 @@ impl ServiceId {
     }
 }
 
+/// UUID of a service.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct ServiceUuid(pub Uuid);
 
@@ -105,6 +161,7 @@ impl fmt::Display for ServiceUuid {
     }
 }
 
+/// Cookie of a service.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[non_exhaustive]
 pub struct ServiceCookie(pub Uuid);
@@ -115,10 +172,18 @@ impl fmt::Display for ServiceCookie {
     }
 }
 
+/// Function call received by a service.
+///
+/// Function calls can be received with the [`Stream`] implementation of [`Service`].
 #[derive(Debug)]
 pub struct FunctionCall {
+    /// Id of the called function.
     pub id: u32,
+
+    /// Arguments passed to called function.
     pub args: Value,
+
+    /// Used to set the reply of a function call.
     pub reply: FunctionCallReply,
 }
 
@@ -132,6 +197,10 @@ impl FunctionCall {
     }
 }
 
+/// Helper used to reply to a pending service function call.
+///
+/// Every [`FunctionCall`] caries a reply object as well. It can be used once to set the reply of
+/// the function call.
 #[derive(Debug)]
 pub struct FunctionCallReply {
     client: Option<Handle>,
@@ -146,6 +215,7 @@ impl FunctionCallReply {
         }
     }
 
+    /// Signal that the function call was successful.
     pub async fn ok(mut self, res: Value) -> Result<(), Error> {
         self.client
             .take()
@@ -154,6 +224,7 @@ impl FunctionCallReply {
             .await
     }
 
+    /// Signal that the function call failed.
     pub async fn err(mut self, res: Value) -> Result<(), Error> {
         self.client
             .take()
@@ -162,6 +233,9 @@ impl FunctionCallReply {
             .await
     }
 
+    /// Abort the function call.
+    ///
+    /// The caller will be still be notified that the call was aborted.
     pub async fn abort(mut self) -> Result<(), Error> {
         self.client
             .take()
@@ -170,6 +244,10 @@ impl FunctionCallReply {
             .await
     }
 
+    /// Signal that an invalid function has been called.
+    ///
+    /// The function itself is identified by [`FunctionCall::id`], which might be invalid or
+    /// unexpected by the service.
     pub async fn invalid_function(mut self) -> Result<(), Error> {
         self.client
             .take()
@@ -178,6 +256,7 @@ impl FunctionCallReply {
             .await
     }
 
+    /// Signal that a invalid arguments were passed to the function.
     pub async fn invalid_args(mut self) -> Result<(), Error> {
         self.client
             .take()
