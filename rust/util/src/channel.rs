241869d32b9e284a9ffe1344aae3788eaea33d64
Dennis Hamester <dennis.hamester@gmail.com>
2020-02-18 07:08:07 +0100

    util: channel: Add UnboundedTransport

--- rust/util/src/channel.rs
+++ rust/util/src/channel.rs
@@ -99,6 +99,98 @@ impl Transport for ChannelTransport {
     }
 }
 
+pub fn unbounded() -> (UnboundedTransport, UnboundedTransport) {
+    let (sender1, receiver1) = mpsc::unbounded();
+    let (sender2, receiver2) = mpsc::unbounded();
+
+    (
+        UnboundedTransport::new(None, receiver1, sender2),
+        UnboundedTransport::new(None, receiver2, sender1),
+    )
+}
+
+pub fn unbounded_with_name(name: String) -> (UnboundedTransport, UnboundedTransport) {
+    let (sender1, receiver1) = mpsc::unbounded();
+    let (sender2, receiver2) = mpsc::unbounded();
+
+    (
+        UnboundedTransport::new(Some(name.clone()), receiver1, sender2),
+        UnboundedTransport::new(Some(name), receiver2, sender1),
+    )
+}
+
+#[derive(Debug)]
+pub struct UnboundedTransport {
+    name: Option<String>,
+    receiver: mpsc::UnboundedReceiver<Message>,
+    sender: mpsc::UnboundedSender<Message>,
+}
+
+impl UnboundedTransport {
+    fn new(
+        name: Option<String>,
+        receiver: mpsc::UnboundedReceiver<Message>,
+        sender: mpsc::UnboundedSender<Message>,
+    ) -> Self {
+        UnboundedTransport {
+            name,
+            receiver,
+            sender,
+        }
+    }
+}
+
+impl Stream for UnboundedTransport {
+    type Item = Result<Message, Closed>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> Poll<Option<Result<Message, Closed>>> {
+        Pin::new(&mut self.receiver)
+            .poll_next(cx)
+            .map(|msg| msg.map(Ok))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.receiver.size_hint()
+    }
+}
+
+impl Sink<Message> for UnboundedTransport {
+    type Error = Closed;
+
+    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Closed>> {
+        Pin::new(&mut self.sender)
+            .poll_ready(cx)
+            .map(map_poll_send_error)
+    }
+
+    fn start_send(mut self: Pin<&mut Self>, item: Message) -> Result<(), Closed> {
+        Pin::new(&mut self.sender)
+            .start_send(item)
+            .map_err(map_send_error)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Closed>> {
+        Pin::new(&mut self.sender)
+            .poll_flush(cx)
+            .map(map_poll_send_error)
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Closed>> {
+        Pin::new(&mut self.sender)
+            .poll_close(cx)
+            .map(map_poll_send_error)
+    }
+}
+
+impl Transport for UnboundedTransport {
+    fn name(&self) -> Option<&str> {
+        self.name.as_deref()
+    }
+}
+
 /// Error type for channel transports.
 ///
 /// Channel transports fail only when either end has been closed or dropped.
