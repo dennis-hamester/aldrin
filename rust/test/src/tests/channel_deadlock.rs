0004c124b135d118102042a8c8c03fd96f3579c9
Dennis Hamester <dennis.hamester@gmail.com>
2020-04-28 21:12:06 +0200

    tests: Add channel_deadlock

---
+++ rust/test/src/tests/channel_deadlock.rs
@@ -0,0 +1,36 @@
+use crate::util;
+use aldrin_client::{ObjectUuid, SubscribeMode};
+use anyhow::Result;
+use std::time::Duration;
+use tokio::time::timeout;
+use uuid::Uuid;
+
+const FIFO_SIZE: usize = 4;
+
+// Test that a misbehaving client which doesn't drain all of its messages doesn't deadlock all the
+// way to the broker, effectively deadlocking the whole bus.
+#[tokio::test]
+async fn channel_deadlock() -> Result<()> {
+    let (mut broker, broker_join) = util::create_broker(FIFO_SIZE);
+    let (mut client, client_join, _, conn_join) =
+        util::create_client(broker.clone(), FIFO_SIZE, FIFO_SIZE, FIFO_SIZE).await?;
+
+    // // This is never drained.
+    let _oc = client
+        .objects_created(SubscribeMode::All, FIFO_SIZE)
+        .await?;
+
+    timeout(Duration::from_secs(1), async move {
+        for _ in 0..32 {
+            client.create_object(ObjectUuid(Uuid::new_v4())).await?;
+        }
+
+        broker.shutdown().await;
+        broker_join.await?;
+        client_join.await??;
+        conn_join.await??;
+
+        Ok(())
+    })
+    .await?
+}
