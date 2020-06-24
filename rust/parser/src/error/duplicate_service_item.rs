7d696087fbe9079e7a34a4fdfcfa5bf4c8652723
Dennis Hamester <dennis.hamester@gmail.com>
2020-06-24 12:15:20 +0200

    parser: Improve DuplicateServiceItem error formatting

--- rust/parser/src/error/duplicate_service_item.rs
+++ rust/parser/src/error/duplicate_service_item.rs
@@ -2,46 +2,38 @@ use super::Error;
 use crate::ast::{Ident, ServiceDef};
 use crate::diag::{Diagnostic, DiagnosticKind, Formatted, Formatter};
 use crate::validate::Validate;
-use crate::{Parsed, Span};
-use std::collections::hash_map::{Entry, HashMap};
+use crate::{util, Parsed, Span};
 
 #[derive(Debug)]
 pub struct DuplicateServiceItem {
     schema_name: String,
     duplicate: Ident,
-    original_span: Span,
+    first: Span,
     service_ident: Ident,
 }
 
 impl DuplicateServiceItem {
     pub(crate) fn validate(service: &ServiceDef, validate: &mut Validate) {
-        let mut idents = HashMap::new();
-
-        for item in service.items() {
-            let name = item.name();
-            match idents.entry(name.value()) {
-                Entry::Vacant(e) => {
-                    e.insert(name);
-                }
-
-                Entry::Occupied(e) => {
-                    validate.add_error(DuplicateServiceItem {
-                        schema_name: validate.schema_name().to_owned(),
-                        duplicate: name.clone(),
-                        original_span: e.get().span(),
-                        service_ident: service.name().clone(),
-                    });
-                }
-            }
-        }
+        util::find_duplicates(
+            service.items(),
+            |item| item.name().value(),
+            |duplicate, first| {
+                validate.add_error(DuplicateServiceItem {
+                    schema_name: validate.schema_name().to_owned(),
+                    duplicate: duplicate.name().clone(),
+                    first: first.name().span(),
+                    service_ident: service.name().clone(),
+                })
+            },
+        );
     }
 
     pub fn duplicate(&self) -> &Ident {
         &self.duplicate
     }
 
-    pub fn original_span(&self) -> Span {
-        self.original_span
+    pub fn first(&self) -> Span {
+        self.first
     }
 
     pub fn service_ident(&self) -> &Ident {
@@ -72,18 +64,7 @@ impl Diagnostic for DuplicateServiceItem {
                 self.duplicate.span(),
                 "duplicate defined here",
             )
-            .info_block(
-                schema,
-                self.original_span.from,
-                self.original_span,
-                "first defined here",
-            )
-            .info_block(
-                schema,
-                self.service_ident.span().from,
-                self.service_ident.span(),
-                format!("service `{}` defined here", self.service_ident.value()),
-            );
+            .info_block(schema, self.first.from, self.first, "first defined here");
         }
 
         fmt.format()
