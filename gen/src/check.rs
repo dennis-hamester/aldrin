bb2552eb24834869663c127419c14b3d1ef5b93e
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-08 07:11:58 +0200

    gen: check: Allow checking multiple schemas

--- gen/src/check.rs
+++ gen/src/check.rs
@@ -11,9 +11,9 @@ pub struct CheckArgs {
     #[structopt(flatten)]
     common_read_args: CommonReadArgs,
 
-    /// Path to an Aldrin schema file
-    #[structopt(name = "schema")]
-    file: PathBuf,
+    /// Paths to one or more Aldrin schema files
+    #[structopt(name = "schema", required = true)]
+    files: Vec<PathBuf>,
 }
 
 pub fn run(args: CheckArgs) -> Result<(), ()> {
@@ -23,19 +23,32 @@ pub fn run(args: CheckArgs) -> Result<(), ()> {
         parser.add_schema_path(include);
     }
 
-    let parsed = parser.parse(args.file);
-    diag::print_diagnostics(&parsed, args.common_args.color).ok();
-
-    if parsed.errors().is_empty() {
-        if parsed.warnings().is_empty() {
-            println!("No issues found.");
-        } else {
-            println!("Some warning(s) found.");
+    let mut res = Ok(());
+    let mut first = true;
+    for file in &args.files {
+        if args.files.len() > 1 {
+            if first {
+                first = false;
+            } else {
+                println!();
+            }
+            println!("{}:", file.display());
         }
 
-        Ok(())
-    } else {
-        println!("Some error(s) found.");
-        Err(())
+        let parsed = parser.parse(file);
+        diag::print_diagnostics(&parsed, args.common_args.color).ok();
+
+        if parsed.errors().is_empty() {
+            if parsed.warnings().is_empty() {
+                println!("No issues found.");
+            } else {
+                println!("Some warning(s) found.");
+            }
+        } else {
+            println!("Some error(s) found.");
+            res = Err(());
+        }
     }
+
+    res
 }
