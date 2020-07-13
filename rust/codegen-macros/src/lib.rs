8a8e5e1f4872213a40612a78a5973f019cc8372c
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-13 20:47:00 +0200

    Write documentation for aldrin-codegen-macros

--- rust/codegen-macros/src/lib.rs
+++ rust/codegen-macros/src/lib.rs
@@ -1,4 +1,128 @@
+//! Aldrin code generation macros
+//!
+//! This crate provides a single macro as an alternative frontend to the Aldrin code generator. It
+//! removes the need to generate code from a schema beforehand or as part of a `build.rs`.
+//!
+//! ## Basic usage
+//!
+//! The [`generate!`] macro takes one required argument, the path to the schema file. Paths can be
+//! relative to `Cargo.toml` file. This requires building with Cargo (or more specifically, the
+//! `CARGO_MANIFEST_DIR` environment variable). Building without Cargo currently supports only
+//! absolute paths.
+//!
+//! The generated code depends only the `aldrin-client` crate. Make sure you have it specified as a
+//! dependency in your `Cargo.toml`.
+//!
+//! ```
+//! aldrin_codegen_macros::generate!("schemas/example1.aldrin");
+//!
+//! fn main() {
+//!     example1::MyStruct::builder()
+//!         .set_field1(Some(12))
+//!         .set_field2(Some(34))
+//!         .build();
+//! }
+//! ```
+//!
+//! This generates the module `example1` with the same content as if the stand-alone code generator
+//! was used.
+//!
+//! The module has `pub` visibility, which is not always desired, especially in library crates. A
+//! common pattern is to put the generated modules inside an additional `schemas` module:
+//!
+//! ```
+//! mod schemas {
+//!     aldrin_codegen_macros::generate!("schemas/example1.aldrin");
+//! }
+//! ```
+//!
+//! If you have only a single schema, it is occasionally convenient to put the generated code inside
+//! another module (like above), but then also re-export everything into it:
+//!
+//! ```
+//! mod schema {
+//!     aldrin_codegen_macros::generate!("schemas/example1.aldrin");
+//!     pub use example1::*;
+//! }
+//!
+//! fn main() {
+//!     schema::MyStruct::builder() // Note `schema` instead of `example1`.
+//!         .set_field1(Some(12))
+//!         .set_field2(Some(34))
+//!         .build();
+//! }
+//! ```
+//!
+//! ## Multiple schemas
+//!
+//! It is possible to pass additional paths to the macro. Code will then be generated for all of
+//! them:
+//!
+//! ```
+//! aldrin_codegen_macros::generate! {
+//!     "schemas/example1.aldrin",
+//!     "schemas/example2.aldrin",
+//! }
+//! # fn main() {}
+//! ```
+//!
+//! Any additional options (see below) will be applied to all schemas. If this is not desired, then
+//! the macro can be called multiple times instead.
+//!
+//! ## Include directories
+//!
+//! You can specify include directories with `include = "path"`:
+//!
+//! ```
+//! aldrin_codegen_macros::generate! {
+//!     "schemas/example3.aldrin",
+//!     "schemas/example4.aldrin",
+//!     include = "schemas",
+//! }
+//!
+//! fn main() {
+//!     example3::Foo::builder()
+//!         .set_bar(Some(example4::Bar::builder().set_baz(Some(12)).build()))
+//!         .build();
+//! }
+//! ```
+//!
+//! The `include` option can be repeated multiple times.
+//!
+//! ## Errors and warnings
+//!
+//! Any errors and warnings from the schemas will be shown as part of the regular compiler
+//! output. No code will be generated, if there are any errors in the schemas.
+//!
+//! Warnings can currently only be emitted on nightly Rust. They are silently ignored on stable and
+//! beta. Unfortunately, this may suppress important diagnostics about your schemas. You can use the
+//! option `warnings_as_errors = true` to treat all warnings as errors.
+//!
+//! On the other hand, if you are on nightly Rust and generate code for a foreign schema, which you
+//! have no direct influence on, warnings may clutter your compiler output. In that case you can use
+//! `suppress_warnings = true` to ignore all warnings.
+//!
+//! In general, it is advisable to use `warnings_as_errors` for your own schemas, and
+//! `suppress_warnings` for foreign schemas:
+//!
+//! ```
+//! // Own schema
+//! aldrin_codegen_macros::generate! {
+//!     "schemas/example5.aldrin",
+//!     include = "schemas/foreign",
+//!     warnings_as_errors = true,
+//! }
+//!
+//! // Foreign schema
+//! aldrin_codegen_macros::generate! {
+//!     "schemas/foreign/example6.aldrin",
+//!     suppress_warnings = true,
+//! }
+//! # fn main() {}
+//! ```
+
 #![deny(intra_doc_link_resolution_failure)]
+#![deny(missing_docs)]
 
 extern crate proc_macro;
 
@@ -15,6 +139,9 @@ use std::path::PathBuf;
 use syn::parse::{Parse, ParseStream};
 use syn::{parse_macro_input, Error, Ident, LitBool, LitStr, Result, Token};
 
+/// Generates code from an Aldrin schema.
+///
+/// See the [crate-level documentation](crate) for more information.
 #[proc_macro_error]
 #[proc_macro]
 pub fn generate(input: TokenStream) -> TokenStream {
