6f0a00f65d9337c40a33eb58620a9c9146fa8cca
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-23 08:13:11 +0200

    client: Improve documentation of objects module

--- rust/client/src/objects.rs
+++ rust/client/src/objects.rs
@@ -4,6 +4,104 @@ use futures_core::stream::{FusedStream, Stream};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
+/// Stream of object creation and destruction events.
+///
+/// [`Objects`] is created with [`Handle::objects`](crate::Handle::objects) and can be used to
+/// discover and track [`Object`s](crate::Object) on the bus.
+///
+/// If [`Objects`] is created with
+/// [`SubscribeMode::CurrentOnly`](crate::SubscribeMode::CurrentOnly), then the stream will
+/// automatically end (return `None`) after it has returned [`ObjectId`s](ObjectId) for all current
+/// [`Object`s](crate::Object) on the bus.
+///
+/// If using either [`SubscribeMode::NewOnly`](crate::SubscribeMode::NewOnly) or
+/// [`SubscribeMode::All`](crate::SubscribeMode::All), then the stream will end only when the
+/// [`Client`](crate::Client) is shut down.
+///
+/// # Examples
+/// ```
+/// use aldrin_client::{ObjectEvent, SubscribeMode};
+/// use futures::stream::StreamExt;
+///
+/// # #[tokio::main]
+/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # let broker = aldrin_broker::Broker::new();
+/// # let handle = broker.handle().clone();
+/// # tokio::spawn(broker.run());
+/// # let (async_transport, t2) = aldrin_util::channel::unbounded();
+/// # let conn = tokio::spawn(async move { handle.add_connection(t2).await });
+/// # let client = aldrin_client::Client::connect(async_transport).await?;
+/// # let handle = client.handle().clone();
+/// # tokio::spawn(client.run());
+/// # tokio::spawn(conn.await??.run());
+/// let mut objects = handle.objects(SubscribeMode::CurrentOnly)?;
+/// # handle.create_object(aldrin_client::ObjectUuid::new_v4()).await?;
+///
+/// while let Some(event) = objects.next().await {
+///     match event {
+///         ObjectEvent::Created(object_id) => {
+///             println!("Object created: {:?}.", object_id);
+///         }
+///
+///         ObjectEvent::Destroyed(object_id) => {
+///             println!("Object destroyed: {:?}.", object_id);
+///         }
+///     }
+/// }
+/// # Ok(())
+/// # }
+/// ```
+///
+/// [`Objects`] can be used to easily search for a particular [`Object`](crate::Object) and acquire
+/// its [`ObjectId`]:
+///
+/// ```
+/// use aldrin_client::{ObjectEvent, ObjectUuid, SubscribeMode};
+/// use futures::future;
+/// use futures::stream::StreamExt;
+/// use uuid::Uuid;
+///
+/// // d434e9c8-6230-4fa6-b61c-1babbaa37a4f
+/// const INTERESTING_OBJECT_UUID: ObjectUuid =
+///     ObjectUuid(Uuid::from_u128(0xd434e9c862304fa6b61c1babbaa37a4f));
+///
+/// # #[tokio::main]
+/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # let broker = aldrin_broker::Broker::new();
+/// # let handle = broker.handle().clone();
+/// # tokio::spawn(broker.run());
+/// # let (async_transport, t2) = aldrin_util::channel::unbounded();
+/// # let conn = tokio::spawn(async move { handle.add_connection(t2).await });
+/// # let client = aldrin_client::Client::connect(async_transport).await?;
+/// # let handle = client.handle().clone();
+/// # tokio::spawn(client.run());
+/// # tokio::spawn(conn.await??.run());
+/// # let obj = handle.create_object(INTERESTING_OBJECT_UUID).await?;
+/// let mut objects = handle.objects(SubscribeMode::CurrentOnly)?;
+///
+/// let object_id = objects
+///     .filter_map(|event| {
+///         future::ready(match event {
+///             ObjectEvent::Created(object_id) if object_id.uuid == INTERESTING_OBJECT_UUID => {
+///                 Some(object_id)
+///             }
+///             _ => None,
+///         })
+///     })
+///     .next()
+///     .await;
+///
+/// # assert!(object_id.is_some());
+/// if let Some(object_id) = object_id {
+///     # assert_eq!(object_id.uuid, INTERESTING_OBJECT_UUID);
+///     println!("Object {} found.", INTERESTING_OBJECT_UUID);
+///     // Do something with object_id here ...
+/// } else {
+///     println!("Object {} not found.", INTERESTING_OBJECT_UUID);
+/// }
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Debug)]
 #[must_use = "streams do nothing unless you poll them"]
 pub struct Objects(mpsc::UnboundedReceiver<ObjectEvent>);
@@ -32,8 +130,15 @@ impl FusedStream for Objects {
     }
 }
 
+/// Event about a created or destroyed object.
+///
+/// This is the element type of the [`Objects`] stream. See that type for more information and usage
+/// examples.
 #[derive(Debug, Copy, Clone)]
 pub enum ObjectEvent {
+    /// An object with the specified id was created.
     Created(ObjectId),
+
+    /// An object with the specified id was destroyed.
     Destroyed(ObjectId),
 }
