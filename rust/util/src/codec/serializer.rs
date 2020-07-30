1de0770e8b71e17ccbcf878f7eae7da5d37a751e
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-30 07:28:31 +0200

    util: Implement Serializer for &mut T and Box<T>

--- rust/util/src/codec/serializer.rs
+++ rust/util/src/codec/serializer.rs
@@ -7,3 +7,27 @@ pub trait Serializer {
     fn serialize(&mut self, msg: Message, dst: &mut BytesMut) -> Result<(), Self::Error>;
     fn deserialize(&mut self, src: Bytes) -> Result<Message, Self::Error>;
 }
+
+impl<T: Serializer + ?Sized> Serializer for &mut T {
+    type Error = T::Error;
+
+    fn serialize(&mut self, msg: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        (*self).serialize(msg, dst)
+    }
+
+    fn deserialize(&mut self, src: Bytes) -> Result<Message, Self::Error> {
+        (*self).deserialize(src)
+    }
+}
+
+impl<T: Serializer + ?Sized> Serializer for Box<T> {
+    type Error = T::Error;
+
+    fn serialize(&mut self, msg: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        (**self).serialize(msg, dst)
+    }
+
+    fn deserialize(&mut self, src: Bytes) -> Result<Message, Self::Error> {
+        (**self).deserialize(src)
+    }
+}
