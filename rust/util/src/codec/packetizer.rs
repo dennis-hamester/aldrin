e94fa72a1242e589e6dd427c02ecd18c59877537
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-30 07:28:09 +0200

    util: Implement Packetizer for &mut T and Box<T>

--- rust/util/src/codec/packetizer.rs
+++ rust/util/src/codec/packetizer.rs
@@ -6,3 +6,27 @@ pub trait Packetizer {
     fn encode(&mut self, data: Bytes, dst: &mut BytesMut) -> Result<(), Self::Error>;
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Bytes>, Self::Error>;
 }
+
+impl<T: Packetizer + ?Sized> Packetizer for &mut T {
+    type Error = T::Error;
+
+    fn encode(&mut self, data: Bytes, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        (*self).encode(data, dst)
+    }
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Bytes>, Self::Error> {
+        (*self).decode(src)
+    }
+}
+
+impl<T: Packetizer + ?Sized> Packetizer for Box<T> {
+    type Error = T::Error;
+
+    fn encode(&mut self, data: Bytes, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        (**self).encode(data, dst)
+    }
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Bytes>, Self::Error> {
+        (**self).decode(src)
+    }
+}
