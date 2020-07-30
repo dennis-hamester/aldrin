a5733adf1263bbbdd9eaa856739d65c955e25373
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-30 08:11:56 +0200

    util: Add SerializerExt trait with map_err

--- rust/util/src/codec.rs
+++ rust/util/src/codec.rs
@@ -3,11 +3,11 @@ mod bincode;
 #[cfg(feature = "json")]
 mod json;
 mod length_prefixed;
-mod serializer;
 #[cfg(feature = "tokio-io")]
 mod tokio_io;
 
 pub mod packetizer;
+pub mod serializer;
 
 #[cfg(feature = "bincode-serializer")]
 pub use self::bincode::{BincodeError, BincodeSerializer};
@@ -15,7 +15,7 @@ pub use self::bincode::{BincodeError, BincodeSerializer};
 pub use json::{JsonError, JsonSerializer};
 pub use length_prefixed::{LengthPrefixed, LengthPrefixedBuilder, LengthPrefixedError};
 pub use packetizer::{Packetizer, PacketizerExt};
-pub use serializer::Serializer;
+pub use serializer::{Serializer, SerializerExt};
 #[cfg(feature = "tokio-io")]
 pub use tokio_io::{TokioCodec, TokioCodecError};
 
--- rust/util/src/codec/serializer.rs
+++ rust/util/src/codec/serializer.rs
@@ -31,3 +31,42 @@ impl<T: Serializer + ?Sized> Serializer for Box<T> {
         (**self).deserialize(src)
     }
 }
+
+pub trait SerializerExt: Serializer {
+    fn map_err<F, E>(self, f: F) -> MapError<Self, F>
+    where
+        Self: Sized,
+        F: FnMut(Self::Error) -> E,
+    {
+        MapError {
+            map_err: f,
+            serializer: self,
+        }
+    }
+}
+
+impl<T: Serializer + ?Sized> SerializerExt for T {}
+
+#[derive(Debug)]
+pub struct MapError<T: ?Sized, F> {
+    map_err: F,
+    serializer: T,
+}
+
+impl<T, F, E> Serializer for MapError<T, F>
+where
+    T: Serializer + ?Sized,
+    F: FnMut(T::Error) -> E,
+{
+    type Error = E;
+
+    fn serialize(&mut self, msg: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        self.serializer
+            .serialize(msg, dst)
+            .map_err(&mut self.map_err)
+    }
+
+    fn deserialize(&mut self, src: Bytes) -> Result<Message, Self::Error> {
+        self.serializer.deserialize(src).map_err(&mut self.map_err)
+    }
+}
