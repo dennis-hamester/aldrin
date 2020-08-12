d245a494bb5e84bdafaeba5f461ed9003dd4dbb8
Dennis Hamester <dennis.hamester@gmail.com>
2020-08-12 22:16:39 +0200

    broker: Port tests to use aldrin-test crate

--- rust/broker/src/broker/test.rs
+++ rust/broker/src/broker/test.rs
@@ -1,14 +1,14 @@
-use crate::test::TestBroker;
 use aldrin_client::{ObjectUuid, ServiceUuid};
 use aldrin_proto::Value;
+use aldrin_test::tokio_based::TestBroker;
 use futures_util::stream::StreamExt;
 use uuid::Uuid;
 
 #[tokio::test]
 async fn disconnect_during_function_call() {
-    let broker = TestBroker::new();
+    let mut broker = TestBroker::new();
 
-    let client1 = broker.add_client().await;
+    let mut client1 = broker.add_client().await;
     let obj = client1.create_object(ObjectUuid::new_v4()).await.unwrap();
     let mut svc = obj
         .create_service(ServiceUuid(Uuid::new_v4()))
@@ -16,14 +16,13 @@ async fn disconnect_during_function_call() {
         .unwrap();
 
     // client2 calls a function on client1 and disconnects before client1 replies.
-    let client2 = broker.add_client().await;
+    let mut client2 = broker.add_client().await;
     let _ = client2.call_function(svc.id(), 0, Value::None).unwrap();
-    client2.shutdown();
     client2.join().await;
 
     let call = svc.next().await.unwrap();
     call.reply.ok(Value::None).unwrap();
-    client1.shutdown();
+    client1.join().await;
 
     broker.join_idle().await
 }
--- rust/broker/src/lib.rs
+++ rust/broker/src/lib.rs
@@ -5,8 +5,6 @@ mod broker;
 mod conn;
 mod conn_id;
 mod serial_map;
-#[cfg(test)]
-mod test;
 
 pub use broker::{Broker, BrokerError, BrokerHandle};
 pub use conn::{Connection, ConnectionError, ConnectionHandle, EstablishError};
