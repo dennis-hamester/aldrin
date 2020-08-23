64110cf7afee6ae3cc28fe3be2907f2d4c6d7b83
Dennis Hamester <dennis.hamester@gmail.com>
2020-08-23 19:02:48 +0200

    util: Add benchmark for JsonSerializer

--- rust/util/benches/codec.rs
+++ rust/util/benches/codec.rs
@@ -1,7 +1,12 @@
 mod packetizer;
+#[cfg(any(feature = "json"))]
+mod serializer;
 
 use criterion::Criterion;
 
 pub fn run(c: &mut Criterion) {
     packetizer::run(c);
+
+    #[cfg(any(feature = "json"))]
+    serializer::run(c);
 }
---
+++ rust/util/benches/codec/serializer/json.rs
@@ -0,0 +1,61 @@
+use super::MessageSize;
+use aldrin_util::codec::{JsonSerializer, Serializer};
+use criterion::measurement::Measurement;
+use criterion::{BatchSize, BenchmarkGroup, BenchmarkId};
+use std::fmt;
+
+pub fn serialize<M: Measurement>(group: &mut BenchmarkGroup<M>, size: MessageSize) {
+    for &pretty in &[true, false] {
+        let input = JsonInput { size, pretty };
+        group.bench_with_input(
+            BenchmarkId::new("JSON/serialize", input),
+            &input,
+            |b, input| {
+                b.iter_batched(
+                    || {
+                        let json = JsonSerializer::with_pretty(input.pretty);
+                        let msg = input.size.get();
+                        (json, msg)
+                    },
+                    |(mut json, msg)| json.serialize(msg).unwrap(),
+                    BatchSize::SmallInput,
+                );
+            },
+        );
+    }
+}
+
+pub fn deserialize<M: Measurement>(group: &mut BenchmarkGroup<M>, size: MessageSize) {
+    for &pretty in &[true, false] {
+        let input = JsonInput { size, pretty };
+        group.bench_with_input(
+            BenchmarkId::new("JSON/deserialize", input),
+            &input,
+            |b, input| {
+                b.iter_batched(
+                    || {
+                        let mut json = JsonSerializer::with_pretty(input.pretty);
+                        let msg = input.size.get();
+                        let data = json.serialize(msg).unwrap().freeze();
+                        (json, data)
+                    },
+                    |(mut json, data)| json.deserialize(data).unwrap(),
+                    BatchSize::SmallInput,
+                );
+            },
+        );
+    }
+}
+
+#[derive(Copy, Clone)]
+struct JsonInput {
+    size: MessageSize,
+    pretty: bool,
+}
+
+impl fmt::Display for JsonInput {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let pretty = if self.pretty { "pretty" } else { "not pretty" };
+        write!(f, "{}/{}", self.size, pretty)
+    }
+}
