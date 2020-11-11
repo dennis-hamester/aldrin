b83b7e1b006286e2673e18a2e52e151f2e849b07
Dennis Hamester <dennis.hamester@gmail.com>
2020-11-11 07:24:18 +0100

    client: codegen: Add utility function get_struct_field

--- rust/client/src/codegen.rs
+++ rust/client/src/codegen.rs
@@ -1,7 +1,35 @@
-//! Re-exports for the code generator.
+//! Re-exports and utilities for the code generator.
 //!
 //! This module and its contents are not part of the public API.
 
 pub use aldrin_proto;
 pub use futures_core;
 pub use uuid;
+
+use aldrin_proto::{FromValue, Value};
+use std::collections::HashMap;
+
+pub fn get_struct_field<T: FromValue>(
+    map: &mut HashMap<u32, Value>,
+    idx: u32,
+    dst: &mut Option<T>,
+    required: bool,
+) -> bool {
+    match map.remove(&idx) {
+        Some(field) => match field.convert() {
+            Ok(field) => {
+                *dst = Some(field);
+                true
+            }
+
+            Err(e) => {
+                if let Some(v) = e.0 {
+                    map.insert(idx, v);
+                }
+                false
+            }
+        },
+
+        None => !required,
+    }
+}
