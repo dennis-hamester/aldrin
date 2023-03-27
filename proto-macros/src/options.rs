c63c133fac2ed70f18e9d26bf2775985263bd402
Dennis Hamester <dennis.hamester@gmail.com>
2023-03-27 12:11:52 +0200

    proto-macros: Update Syn to 2.0.10

--- proto-macros/src/options.rs
+++ proto-macros/src/options.rs
@@ -1,9 +1,5 @@
-use std::fmt::Display;
-use std::str::FromStr;
 use syn::punctuated::Punctuated;
-use syn::{
-    Attribute, Error, Lit, LitInt, LitStr, Meta, NestedMeta, Path, Result, Token, WherePredicate,
-};
+use syn::{Attribute, Error, LitInt, LitStr, Path, Result, Token, WherePredicate};
 
 pub struct Options {
     krate: Path,
@@ -19,40 +15,44 @@ impl Options {
         let mut de_bounds = None;
         let mut deny_unknown_fields = false;
 
-        parse_nested_metas_with(attrs, |meta| match meta {
-            NestedMeta::Meta(Meta::NameValue(nv)) if nv.path.is_ident("crate") => {
-                krate = Some(parse_lit_into_path(&nv.lit)?);
-                Ok(())
+        for attr in attrs {
+            if !attr.path().is_ident("aldrin") {
+                continue;
             }
 
-            NestedMeta::Meta(Meta::NameValue(nv)) if nv.path.is_ident("bounds") => {
-                ser_bounds = Some(parse_lit_into_where_predicates(&nv.lit)?);
-                de_bounds = ser_bounds.clone();
-                Ok(())
-            }
-
-            NestedMeta::Meta(Meta::NameValue(nv)) if nv.path.is_ident("ser_bounds") => {
-                ser_bounds = Some(parse_lit_into_where_predicates(&nv.lit)?);
-                Ok(())
-            }
-
-            NestedMeta::Meta(Meta::NameValue(nv)) if nv.path.is_ident("de_bounds") => {
-                de_bounds = Some(parse_lit_into_where_predicates(&nv.lit)?);
-                Ok(())
-            }
-
-            NestedMeta::Meta(Meta::NameValue(nv)) if nv.path.is_ident("de_bounds") => {
-                deny_unknown_fields = true;
-                Ok(())
-            }
-
-            NestedMeta::Meta(Meta::Path(path)) if path.is_ident("deny_unknown_fields") => {
-                deny_unknown_fields = true;
-                Ok(())
-            }
-
-            _ => Err(Error::new_spanned(meta, "unknown attribute")),
-        })?;
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("crate") {
+                    let value: LitStr = meta.value()?.parse()?;
+                    krate = value
+                        .parse()
+                        .map(Some)
+                        .map_err(|e| Error::new_spanned(value, e))?;
+                    Ok(())
+                } else if meta.path.is_ident("bounds") {
+                    let value: LitStr = meta.value()?.parse()?;
+                    ser_bounds = parse_lit_str_into_where_predicates(&value).map(Some)?;
+                    de_bounds = ser_bounds.clone();
+                    Ok(())
+                } else if meta.path.is_ident("ser_bounds") {
+                    let value: LitStr = meta.value()?.parse()?;
+                    ser_bounds = parse_lit_str_into_where_predicates(&value).map(Some)?;
+                    Ok(())
+                } else if meta.path.is_ident("de_bounds") {
+                    let value: LitStr = meta.value()?.parse()?;
+                    de_bounds = parse_lit_str_into_where_predicates(&value).map(Some)?;
+                    Ok(())
+                } else if meta.path.is_ident("deny_unknown_fields") {
+                    if meta.input.is_empty() {
+                        deny_unknown_fields = true;
+                        Ok(())
+                    } else {
+                        Err(meta.input.error("unexpected extra tokens"))
+                    }
+                } else {
+                    Err(meta.error("unknown attribute"))
+                }
+            })?;
+        }
 
         Ok(Self {
             krate: krate.unwrap_or_else(|| syn::parse_quote!(aldrin_proto)),
@@ -87,14 +87,21 @@ impl ItemOptions {
     pub fn new(attrs: &[Attribute], default_id: u32) -> Result<Self> {
         let mut id = default_id;
 
-        parse_nested_metas_with(attrs, |meta| match meta {
-            NestedMeta::Meta(Meta::NameValue(nv)) if nv.path.is_ident("id") => {
-                id = parse_lit_int(&nv.lit)?;
-                Ok(())
+        for attr in attrs {
+            if !attr.path().is_ident("aldrin") {
+                continue;
             }
 
-            _ => Err(Error::new_spanned(meta, "unknown attribute")),
-        })?;
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("id") {
+                    let value: LitInt = meta.value()?.parse()?;
+                    id = value.base10_parse()?;
+                    Ok(())
+                } else {
+                    Err(meta.error("unknown attribute"))
+                }
+            })?;
+        }
 
         Ok(Self { id })
     }
@@ -104,68 +111,11 @@ impl ItemOptions {
     }
 }
 
-fn parse_nested_metas_with<F>(attrs: &[Attribute], mut f: F) -> Result<()>
-where
-    F: FnMut(NestedMeta) -> Result<()>,
-{
-    for attr in attrs {
-        if !attr.path.is_ident("aldrin") {
-            continue;
-        }
-
-        let metas = get_nested_attr_metas(attr)?;
-
-        for meta in metas {
-            f(meta)?;
-        }
-    }
-
-    Ok(())
-}
-
-fn get_nested_attr_metas(attr: &Attribute) -> Result<Punctuated<NestedMeta, Token![,]>> {
-    let meta = attr.parse_meta()?;
-    let Meta::List(metas) = meta else {
-        return Err(Error::new_spanned(attr, "expected #[aldrin(...)]"));
-    };
-
-    Ok(metas.nested)
-}
-
-fn get_lit_str(lit: &Lit) -> Result<&LitStr> {
-    if let Lit::Str(lit) = lit {
-        Ok(lit)
-    } else {
-        Err(Error::new_spanned(lit, "expected a string literal"))
-    }
-}
-
-fn get_lit_int(lit: &Lit) -> Result<&LitInt> {
-    if let Lit::Int(lit) = lit {
-        Ok(lit)
-    } else {
-        Err(Error::new_spanned(lit, "expected an integer literal"))
-    }
-}
-
-fn parse_lit_into_path(lit: &Lit) -> Result<Path> {
-    let string = get_lit_str(lit)?;
-    string.parse().map_err(|e| Error::new_spanned(string, e))
-}
-
-fn parse_lit_int<T>(lit: &Lit) -> Result<T>
-where
-    T: FromStr,
-    T::Err: Display,
-{
-    let int = get_lit_int(lit)?;
-    int.base10_parse()
-}
-
-fn parse_lit_into_where_predicates(lit: &Lit) -> Result<Punctuated<WherePredicate, Token![,]>> {
-    let string = get_lit_str(lit)?;
+fn parse_lit_str_into_where_predicates(
+    lit_str: &LitStr,
+) -> Result<Punctuated<WherePredicate, Token![,]>> {
     let parser = Punctuated::<WherePredicate, Token![,]>::parse_terminated;
-    string
+    lit_str
         .parse_with(parser)
-        .map_err(|e| Error::new_spanned(string, e))
+        .map_err(|e| Error::new_spanned(lit_str, e))
 }
