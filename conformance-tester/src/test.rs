94bf1ad6a05210ee2a8c519847c5815287823ad6
Dennis Hamester <dennis.hamester@gmail.com>
2023-04-04 07:39:47 +0200

    conformance-tester: tests: Add create-service-duplicate

--- conformance-tester/src/test.rs
+++ conformance-tester/src/test.rs
@@ -48,6 +48,7 @@ pub static BUILT_IN_TESTS: Lazy<Vec<Test>> = Lazy::new(|| {
         include_str!("../tests/connect-version-too-low.json"),
         include_str!("../tests/create-object-duplicate.json"),
         include_str!("../tests/create-object-ok.json"),
+        include_str!("../tests/create-service-duplicate.json"),
         include_str!("../tests/create-service-ok.json"),
         include_str!("../tests/destroy-foreign-object.json"),
         include_str!("../tests/destroy-invalid-object.json"),
