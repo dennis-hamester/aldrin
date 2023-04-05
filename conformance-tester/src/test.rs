4d14424f76a0d265c5e4a544684f2adbe1d44406
Dennis Hamester <dennis.hamester@gmail.com>
2023-04-05 07:20:41 +0200

    conformance-tester: tests: Add destroy-service-ok

--- conformance-tester/src/test.rs
+++ conformance-tester/src/test.rs
@@ -55,6 +55,7 @@ pub static BUILT_IN_TESTS: Lazy<Vec<Test>> = Lazy::new(|| {
         include_str!("../tests/destroy-foreign-object.json"),
         include_str!("../tests/destroy-invalid-object.json"),
         include_str!("../tests/destroy-object-ok.json"),
+        include_str!("../tests/destroy-service-ok.json"),
         include_str!("../tests/enumerate-and-subscribe-objects.json"),
         include_str!("../tests/enumerate-objects-2.json"),
         include_str!("../tests/enumerate-objects-empty.json"),
