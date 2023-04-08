86f6d6cdd308d25fdcfddf58ee43bb90c30c0f3a
Dennis Hamester <dennis.hamester@gmail.com>
2023-04-08 11:41:13 +0200

    conformance-tester: tests: Add emit-event-1-subscriber

--- conformance-tester/src/test.rs
+++ conformance-tester/src/test.rs
@@ -72,6 +72,7 @@ pub static BUILT_IN_TESTS: Lazy<Vec<Test>> = Lazy::new(|| {
         include_str!("../tests/destroy-object-ok.json"),
         include_str!("../tests/destroy-service-ok.json"),
         include_str!("../tests/emit-event-0-subscribers.json"),
+        include_str!("../tests/emit-event-1-subscriber.json"),
         include_str!("../tests/enumerate-and-subscribe-objects.json"),
         include_str!("../tests/enumerate-and-subscribe-services.json"),
         include_str!("../tests/enumerate-objects-2.json"),
