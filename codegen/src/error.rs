248da6a3515f382f09c741ad28a3bfee915a6d33
Dennis Hamester <dennis.hamester@gmail.com>
2023-12-07 19:30:41 +0100

    codegen: Use thiserror

--- codegen/src/error.rs
+++ codegen/src/error.rs
@@ -1,48 +1,24 @@
 use diffy::{ApplyError, ParsePatchError};
-use std::error::Error as StdError;
 use std::fmt;
 use std::io::Error as IoError;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
-    Io(IoError),
-    Subprocess(SubprocessError),
-    ParsePatch(ParsePatchError),
-    ApplyPatch(ApplyError),
+    #[error(transparent)]
+    Io(#[from] IoError),
+
+    #[error(transparent)]
+    Subprocess(#[from] SubprocessError),
+
+    #[error(transparent)]
+    ParsePatch(#[from] ParsePatchError),
+
+    #[error(transparent)]
+    ApplyPatch(#[from] ApplyError),
 }
 
-impl From<IoError> for Error {
-    fn from(e: IoError) -> Self {
-        Error::Io(e)
-    }
-}
-
-impl From<ParsePatchError> for Error {
-    fn from(e: ParsePatchError) -> Self {
-        Error::ParsePatch(e)
-    }
-}
-
-impl From<ApplyError> for Error {
-    fn from(e: ApplyError) -> Self {
-        Error::ApplyPatch(e)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::Io(e) => e.fmt(f),
-            Error::Subprocess(e) => e.fmt(f),
-            Error::ParsePatch(e) => e.fmt(f),
-            Error::ApplyPatch(e) => e.fmt(f),
-        }
-    }
-}
-
-impl StdError for Error {}
-
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub struct SubprocessError {
     pub(crate) command: String,
     pub(crate) code: Option<i32>,
@@ -63,12 +39,6 @@ impl SubprocessError {
     }
 }
 
-impl From<SubprocessError> for Error {
-    fn from(e: SubprocessError) -> Self {
-        Error::Subprocess(e)
-    }
-}
-
 impl fmt::Display for SubprocessError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "subprocess `{}` failed", self.command)?;
@@ -86,5 +56,3 @@ impl fmt::Display for SubprocessError {
         Ok(())
     }
 }
-
-impl StdError for SubprocessError {}
