23646787b4fe0233ee5e145a85153d2be4a8bb3f
Dennis Hamester <dennis.hamester@gmail.com>
2023-12-07 07:35:11 +0100

    core: Use thiserror for {Serialize,Deserialize}Error

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,48 +1,37 @@
-use std::error::Error;
-use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Error, Debug, Copy, Clone, PartialEq, Eq)]
 pub enum SerializeError {
+    #[error("serialized value overflowed")]
     Overflow,
+
+    #[error("more elements serialized than expected")]
     TooManyElements,
+
+    #[error("fewer elements serialized than expected")]
     TooFewElements,
+
+    #[error("too deeply nested")]
     TooDeeplyNested,
 }
 
-impl fmt::Display for SerializeError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::Overflow => f.write_str("serialized value overflowed"),
-            Self::TooManyElements => f.write_str("more elements serialized than expected"),
-            Self::TooFewElements => f.write_str("less elements serialized than expected"),
-            Self::TooDeeplyNested => f.write_str("too deeply nested"),
-        }
-    }
-}
-
-impl Error for SerializeError {}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Error, Debug, Copy, Clone, PartialEq, Eq)]
 pub enum DeserializeError {
+    #[error("invalid serialization")]
     InvalidSerialization,
+
+    #[error("unexpected end of input")]
     UnexpectedEoi,
+
+    #[error("unexpected value type")]
     UnexpectedValue,
+
+    #[error("no more elements")]
     NoMoreElements,
+
+    #[error("too deeply nested")]
     TooDeeplyNested,
+
+    #[error("serialization contains trailing data")]
     TrailingData,
 }
-
-impl fmt::Display for DeserializeError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::InvalidSerialization => f.write_str("invalid serialization"),
-            Self::UnexpectedEoi => f.write_str("unexpected end of input"),
-            Self::UnexpectedValue => f.write_str("unexpected value type"),
-            Self::NoMoreElements => f.write_str("no more elements"),
-            Self::TooDeeplyNested => f.write_str("too deeply nested"),
-            Self::TrailingData => f.write_str("serialization contains trailing data"),
-        }
-    }
-}
-
-impl Error for DeserializeError {}
