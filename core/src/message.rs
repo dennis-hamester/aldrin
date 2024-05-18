f132eb9662be52abdaee3894fcd02bc910734150
Dennis Hamester <dennis.hamester@gmail.com>
2024-05-18 08:12:03 +0200

    core: Add ServiceInfo type

--- core/src/message.rs
+++ core/src/message.rs
@@ -54,7 +54,11 @@ mod sync_reply;
 mod test;
 mod unsubscribe_event;
 
+use crate::error::{DeserializeError, SerializeError};
+use crate::ids::TypeId;
 use crate::serialized_value::SerializedValueSlice;
+use crate::value_deserializer::{Deserialize, Deserializer};
+use crate::value_serializer::{Serialize, Serializer};
 use bytes::BytesMut;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
@@ -635,4 +639,70 @@ impl message_ops::Sealed for Message {}
 enum OptionKind {
     None = 0,
     Some = 1,
+}
+
+#[derive(IntoPrimitive, TryFromPrimitive)]
+#[repr(u32)]
+enum ServiceInfoField {
+    Version = 0,
+    TypeId = 1,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct ServiceInfo {
+    pub version: u32,
+    pub type_id: Option<TypeId>,
+}
+
+impl ServiceInfo {
+    pub fn new(version: u32) -> Self {
+        Self {
+            version,
+            type_id: None,
+        }
+    }
+
+    pub fn with_type_id(version: u32, type_id: TypeId) -> Self {
+        Self {
+            version,
+            type_id: Some(type_id),
+        }
+    }
+}
+
+impl Serialize for ServiceInfo {
+    fn serialize(&self, serializer: Serializer) -> Result<(), SerializeError> {
+        let mut serializer = serializer.serialize_struct(2)?;
+
+        serializer.serialize_field(ServiceInfoField::Version, &self.version)?;
+        serializer.serialize_field(ServiceInfoField::TypeId, &self.type_id)?;
+
+        serializer.finish()
+    }
+}
+
+impl Deserialize for ServiceInfo {
+    fn deserialize(deserializer: Deserializer) -> Result<Self, DeserializeError> {
+        let mut deserializer = deserializer.deserialize_struct()?;
+
+        let mut version = None;
+        let mut type_id = None;
+
+        while deserializer.has_more_fields() {
+            let deserializer = deserializer.deserialize_field()?;
+
+            match deserializer.try_id() {
+                Ok(ServiceInfoField::Version) => version = deserializer.deserialize().map(Some)?,
+                Ok(ServiceInfoField::TypeId) => type_id = deserializer.deserialize()?,
+                Err(_) => deserializer.skip()?,
+            }
+        }
+
+        deserializer.finish_with(|| {
+            Ok(Self {
+                version: version.ok_or(DeserializeError::InvalidSerialization)?,
+                type_id,
+            })
+        })
+    }
 }
