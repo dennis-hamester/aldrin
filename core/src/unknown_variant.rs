1d086fe4ddcf5c92efbc54bb1d6c248230f30d5b
Dennis Hamester <dennis.hamester@gmail.com>
2024-12-13 16:31:18 +0100

    core: Add UnknownVariant::deserialize_as_value()

--- core/src/unknown_variant.rs
+++ core/src/unknown_variant.rs
@@ -1,4 +1,5 @@
 use crate::error::DeserializeError;
+use crate::generic_value::Enum;
 use crate::serialized_value::{SerializedValue, SerializedValueSlice};
 use crate::value_deserializer::Deserialize;
 
@@ -27,5 +28,9 @@ impl UnknownVariant {
 
     pub fn deserialize<T: Deserialize>(&self) -> Result<T, DeserializeError> {
         self.value.deserialize()
+    }
+
+    pub fn deserialize_as_value(&self) -> Result<Enum, DeserializeError> {
+        self.deserialize().map(|value| Enum::new(self.id, value))
     }
 }
