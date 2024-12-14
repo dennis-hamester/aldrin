12ff8b6e24a02840391c6a7ba1ada972f9c96153
Dennis Hamester <dennis.hamester@gmail.com>
2024-12-14 21:19:12 +0100

    parser: Extend DuplicateServiceItem for function and event fallbacks

--- parser/src/test/ui_tests.rs
+++ parser/src/test/ui_tests.rs
@@ -1,6 +1,7 @@
 ui_test!(const_int_not_found);
 ui_test!(did_you_mean_builtin);
 ui_test!(duplicate_enum_variant);
+ui_test!(duplicate_service_item);
 ui_test!(duplicate_struct_field);
 ui_test!(error_empty_enum);
 ui_test!(expected_const_int_found_service);
