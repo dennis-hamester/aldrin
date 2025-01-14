e750ed9ec790fd8a71064894784be9938e725efb
Dennis Hamester <dennis.hamester@gmail.com>
2025-01-14 07:22:11 +0100

    aldrin: Add low_level::PendingReply::id()

--- aldrin/src/low_level/pending_reply.rs
+++ aldrin/src/low_level/pending_reply.rs
@@ -13,15 +13,20 @@ use std::time::Instant;
 #[must_use = "futures do nothing unless you `.await` or poll them"]
 pub struct PendingReply {
     recv: Receiver<Result<(CallFunctionResult, Instant), Error>>,
-    function: u32,
+    id: u32,
 }
 
 impl PendingReply {
     pub(crate) fn new(
         recv: Receiver<Result<(CallFunctionResult, Instant), Error>>,
-        function: u32,
+        id: u32,
     ) -> Self {
-        Self { recv, function }
+        Self { recv, id }
+    }
+
+    /// Returns the pending reply's function id.
+    pub fn id(&self) -> u32 {
+        self.id
     }
 
     /// Cast the reply to a typed [`PendingReply<T, E>`](HlPendingReply).
@@ -41,11 +46,11 @@ impl Future for PendingReply {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         match Pin::new(&mut self.recv).poll(cx) {
             Poll::Ready(Ok(Ok((CallFunctionResult::Ok(t), timestamp)))) => {
-                Poll::Ready(Ok(Reply::new(self.function, timestamp, Ok(t))))
+                Poll::Ready(Ok(Reply::new(self.id, timestamp, Ok(t))))
             }
 
             Poll::Ready(Ok(Ok((CallFunctionResult::Err(e), timestamp)))) => {
-                Poll::Ready(Ok(Reply::new(self.function, timestamp, Err(e))))
+                Poll::Ready(Ok(Reply::new(self.id, timestamp, Err(e))))
             }
 
             Poll::Ready(Ok(Ok((CallFunctionResult::Aborted, _)))) => {
@@ -57,11 +62,11 @@ impl Future for PendingReply {
             }
 
             Poll::Ready(Ok(Ok((CallFunctionResult::InvalidFunction, _)))) => {
-                Poll::Ready(Err(Error::invalid_function(self.function)))
+                Poll::Ready(Err(Error::invalid_function(self.id)))
             }
 
             Poll::Ready(Ok(Ok((CallFunctionResult::InvalidArgs, _)))) => {
-                Poll::Ready(Err(Error::invalid_arguments(self.function, None)))
+                Poll::Ready(Err(Error::invalid_arguments(self.id, None)))
             }
 
             Poll::Ready(Ok(Err(e))) => Poll::Ready(Err(e)),
