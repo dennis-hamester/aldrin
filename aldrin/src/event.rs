b7be1340ad515091cb1d03065ddb669e6e2fdb02
Dennis Hamester <dennis.hamester@gmail.com>
2025-01-19 09:14:11 +0100

    aldrin: Don't store event arguments in an Option

--- aldrin/src/event.rs
+++ aldrin/src/event.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 pub struct Event<T> {
     id: u32,
     timestamp: Instant,
-    args: Option<T>,
+    args: T,
 }
 
 impl<T> Event<T> {
@@ -13,7 +13,7 @@ impl<T> Event<T> {
         Self {
             id,
             timestamp,
-            args: Some(args),
+            args,
         }
     }
 
@@ -28,43 +28,17 @@ impl<T> Event<T> {
     }
 
     /// Returns a reference to the event's arguments.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if the arguments have been [taken](Self::take_args) out.
     pub fn args(&self) -> &T {
-        self.args.as_ref().expect("args were already taken")
+        &self.args
     }
 
     /// Returns a mutable reference to the event's arguments.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if the arguments have been [taken](Self::take_args) out.
     pub fn args_mut(&mut self) -> &mut T {
-        self.args.as_mut().expect("args were already taken")
-    }
-
-    /// Takes out the event's arguments.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if the arguments have already been taken out.
-    pub fn take_args(&mut self) -> T {
-        self.args.take().expect("args were already taken")
+        &mut self.args
     }
 
     /// Converts the event to its arguments.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if the arguments have been [taken](Self::take_args) out.
-    pub fn into_args(mut self) -> T {
-        self.take_args()
-    }
-
-    /// Returns `true`, if the event's arguments have not yet been taken out.
-    pub fn has_args(&self) -> bool {
-        self.args.is_some()
+    pub fn into_args(self) -> T {
+        self.args
     }
 }
