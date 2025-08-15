c11d42e3116828270588b745a783b6fc1089b8cf
Dennis Hamester <dennis.hamester@gmail.com>
2025-08-15 08:44:58 +0200

    core: fuzz: packetizer-extend-from-slice: Also test for OOM

--- core/fuzz/fuzz_targets/packetizer_extend_from_slice.rs
+++ core/fuzz/fuzz_targets/packetizer_extend_from_slice.rs
@@ -5,9 +5,12 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: Vec<Vec<u8>>| {
     let mut packetizer = Packetizer::new();
+    let mut msgs = Vec::new();
 
     for data in data {
         packetizer.extend_from_slice(&data);
-        while let Some(_) = packetizer.next_message() {}
+        while let Some(msg) = packetizer.next_message() {
+            msgs.push(msg);
+        }
     }
 });
