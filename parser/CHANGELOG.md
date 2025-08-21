e60d7d96537201dbc5858a2024b561a4392248ad
Dennis Hamester <dennis.hamester@gmail.com>
2025-08-21 21:45:05 +0200

    parser: Require whitespace after required keyword in struct fields

--- parser/CHANGELOG.md
+++ parser/CHANGELOG.md
@@ -24,6 +24,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Built-in types are no longer suggested in error messages when an external references points to a
   service or const.
 - Fix a crash when a recursive type is used.
+- Require whitespace after the `required` keyword in a struct field.
 
 ## [0.12.0] - 2025-01-26
 
--- parser/src/test/issues.rs
+++ parser/src/test/issues.rs
@@ -14,3 +14,21 @@ fn trimmed_span() {
 fn recursive_types_used() {
     issue!(recursive_types_used);
 }
+
+#[test]
+fn whitespace_after_required() {
+    let parsed = issue!(whitespace_after_required);
+    let schema = parsed.main_schema();
+
+    let foo = &schema
+        .definitions()
+        .iter()
+        .find(|d| d.name().value() == "Foo")
+        .unwrap()
+        .as_struct()
+        .unwrap()
+        .fields()[0];
+
+    assert_eq!(foo.name().value(), "requiredfoo");
+    assert!(!foo.required());
+}
