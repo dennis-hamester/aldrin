9d5ac6a4cc815605c1d24024786301467b33627c
Dennis Hamester <dennis.hamester@gmail.com>
2025-09-10 20:46:41 +0200

    parser: Add a fuzzer

---
+++ parser/fuzz/fuzz_targets/multi.rs
@@ -0,0 +1,62 @@
+#![no_main]
+
+use aldrin_parser::diag::Renderer;
+use aldrin_parser::{Parsed, Parser};
+use libfuzzer_sys::{fuzz_target, Corpus};
+use std::collections::HashMap;
+use std::fs;
+
+fn gen_diagnostics(parsed: &Parsed) -> String {
+    let renderer = Renderer::new(true, true, 100);
+    let mut diag = String::new();
+
+    for error in parsed.errors() {
+        let rendered = renderer.render(error, parsed);
+        diag.push_str(&rendered);
+    }
+
+    for warning in parsed.warnings() {
+        let rendered = renderer.render(warning, parsed);
+        diag.push_str(&rendered);
+    }
+
+    for warning in parsed.other_warnings() {
+        let rendered = renderer.render(warning, parsed);
+        diag.push_str(&rendered);
+    }
+
+    diag
+}
+
+fuzz_target!(|schemas: HashMap<String, String>| -> Corpus {
+    if schemas.is_empty()
+        || schemas
+            .keys()
+            .any(|s| s.is_empty() || s.contains(['/', '.']))
+    {
+        return Corpus::Reject;
+    }
+
+    let dir = tempfile::tempdir().unwrap();
+    let mut main_schema = None;
+
+    for (schema_name, schema) in &schemas {
+        let mut path = dir.path().to_path_buf();
+        path.push(schema_name);
+        path.set_extension(".aldrin");
+
+        let _ = fs::write(&path, schema);
+
+        if main_schema.is_none() {
+            main_schema = Some(path);
+        }
+    }
+
+    let mut parser = Parser::new();
+    parser.add_schema_path(dir.path());
+
+    let parsed = parser.parse(main_schema.unwrap());
+    let _ = gen_diagnostics(&parsed);
+
+    Corpus::Keep
+});
---
+++ parser/fuzz/fuzz_targets/single.rs
@@ -0,0 +1,40 @@
+#![no_main]
+
+use aldrin_parser::diag::Renderer;
+use aldrin_parser::{Parsed, Parser};
+use libfuzzer_sys::fuzz_target;
+use std::io::Write;
+use tempfile::NamedTempFile;
+
+fn gen_diagnostics(parsed: &Parsed) -> String {
+    let renderer = Renderer::new(true, true, 100);
+    let mut diag = String::new();
+
+    for error in parsed.errors() {
+        let rendered = renderer.render(error, parsed);
+        diag.push_str(&rendered);
+    }
+
+    for warning in parsed.warnings() {
+        let rendered = renderer.render(warning, parsed);
+        diag.push_str(&rendered);
+    }
+
+    for warning in parsed.other_warnings() {
+        let rendered = renderer.render(warning, parsed);
+        diag.push_str(&rendered);
+    }
+
+    diag
+}
+
+fuzz_target!(|schema: String| {
+    let mut file = NamedTempFile::new().unwrap();
+
+    file.write_all(schema.as_bytes()).unwrap();
+    file.flush().unwrap();
+
+    let parser = Parser::new();
+    let parsed = parser.parse(file.path());
+    let _ = gen_diagnostics(&parsed);
+});
