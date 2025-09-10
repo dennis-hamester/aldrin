c2ae9671a03e1db56b34a496b43922b5e7ba3632
Dennis Hamester <dennis.hamester@gmail.com>
2025-09-10 21:26:47 +0200

    parser: Remove Span::lines() and SpanLines

--- parser/src/span.rs
+++ parser/src/span.rs
@@ -1,7 +1,5 @@
 use crate::grammar::Rule;
 use pest::iterators::Pair;
-use std::iter::Skip;
-use std::str::Lines;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct LineCol {
@@ -77,53 +75,6 @@ impl Span {
         Self {
             from: Position::dummy(),
             to: Position::dummy(),
-        }
-    }
-
-    pub fn lines(self, text: &str) -> SpanLines<'_> {
-        SpanLines {
-            span: self,
-            lines: text.lines().skip(self.from.line_col.line - 1),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct SpanLines<'a> {
-    span: Span,
-    lines: Skip<Lines<'a>>,
-}
-
-impl<'a> Iterator for SpanLines<'a> {
-    type Item = (&'a str, Span);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.span.from == self.span.to {
-            None
-        } else if self.span.from.line_col.line < self.span.to.line_col.line {
-            let line = self.lines.next()?;
-
-            let span = Span {
-                from: self.span.from,
-                to: Position {
-                    index: self.span.from.index + line.len(),
-                    line_col: LineCol {
-                        line: self.span.from.line_col.line,
-                        column: line.len() + 1,
-                    },
-                },
-            };
-
-            self.span.from.index = 0;
-            self.span.from.line_col.line += 1;
-            self.span.from.line_col.column = 1;
-
-            Some((line, span))
-        } else {
-            let line = self.lines.next()?;
-            let span = self.span;
-            self.span.from = self.span.to;
-            Some((line, span))
         }
     }
 }
