f9a05f138126d9b9e875a04030847a728048fc8f
Dennis Hamester <dennis.hamester@gmail.com>
2026-07-06 07:10:54 +0200

    parser2: Add a UI test for the UnusedImport warning

--- parser2/src/test.rs
+++ parser2/src/test.rs
@@ -7,10 +7,28 @@ macro_rules! fmt_test {
     };
 }
 
-mod fmt_tests;
+macro_rules! ui_test {
+    ($name:ident) => {
+        ui_test!($name: $name);
+    };
 
-use crate::{Formatter, MemoryResolver, Parser};
-use std::fs;
+    ($name:ident: $ident:ident) => {
+        #[test]
+        fn $ident() {
+            $crate::test::ui_test_impl(stringify!($name));
+        }
+    };
+}
+
+mod fmt_tests;
+mod ui_tests;
+
+use crate::{
+    Diagnostic, DiagnosticRenderer, FilesystemResolver, Formatter, MemoryResolver, Parser,
+};
+use std::collections::HashSet;
+use std::fs::{self, File};
+use std::io::Read;
 use std::path::PathBuf;
 
 fn fmt_test_impl(name: &str) {
@@ -31,4 +49,51 @@ fn fmt_test_impl(name: &str) {
         "{}",
         diffy::create_patch(&source, &formatted).to_string(),
     );
+}
+
+fn ui_test_impl(name: &str) {
+    let base_path: PathBuf = ["test", "ui", name].iter().collect();
+
+    let mut schema_path = base_path.clone();
+    schema_path.set_extension("aldrin");
+
+    let parser = Parser::new(FilesystemResolver::with_include_paths(
+        schema_path,
+        ["test/ui"],
+    ));
+
+    let mut expected = HashSet::new();
+    if base_path.is_dir() {
+        for entry in fs::read_dir(&base_path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            let mut file = File::open(path).unwrap();
+            let mut diag = String::new();
+            file.read_to_string(&mut diag).unwrap();
+            expected.insert(diag);
+        }
+    }
+
+    let mut fail = false;
+
+    let errors = parser.errors().iter().map(|d| d as &dyn Diagnostic);
+    let warnings = parser.warnings().iter().map(|d| d as &dyn Diagnostic);
+    let others = parser.other_warnings().iter().map(|d| d as &dyn Diagnostic);
+    let renderer = DiagnosticRenderer::new(false, true, 100);
+
+    for diag in errors.chain(warnings).chain(others) {
+        let rendered = renderer.render(diag, &parser);
+
+        if !expected.remove(&rendered) {
+            eprintln!("Unexpected diagnostic:\n{rendered}\n");
+            fail = true;
+        }
+    }
+
+    for diag in expected {
+        eprintln!("Expected diagnostic:\n{diag}\n");
+        fail = true;
+    }
+
+    assert!(!fail, "UI test `{name}` encountered unmatched diagnostics");
 }
