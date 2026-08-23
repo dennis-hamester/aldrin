f4cf64fa309d577c5a92f5755c7aa35459f8beda
Dennis Hamester <dennis.hamester@gmail.com>
2026-08-23 15:53:45 +0200

    macros: Update Syn to version 3

--- macros/src/derive/replace_self_ty.rs
+++ macros/src/derive/replace_self_ty.rs
@@ -5,7 +5,11 @@ struct ReplaceSelfTy(Type);
 
 impl VisitMut for ReplaceSelfTy {
     fn visit_type_mut(&mut self, i: &mut Type) {
-        if let Type::Path(TypePath { qself: None, path }) = i
+        if let Type::Path(TypePath {
+            attrs: _,
+            qself: None,
+            path,
+        }) = i
             && path.is_ident("Self")
         {
             *i = self.0.clone();
